aea52dfd0335120be9085da0432e46ee11ecb404
Walber <walberoc@gmail.com>
2024-06-12 07:25:01 -0300

    criando a classe pai para depois extendê-la

---
+++ src/br/com/dio/desafio/dominio/Conteudo.java
@@ -0,0 +1,33 @@
+package br.com.dio.desafio.dominio;
+
+public abstract class Conteudo {
+    // constante
+    protected static final double XP_PADRAO = 10;
+    /* static - posso acessar fora da classe 
+     * protected - filhos de conteúdo terão acesso
+    */
+    private String titulo;
+    private String descricao;
+
+    /* obrigar os filhos a implementar */
+    public abstract double calcularXp();/*  
+                causa de a classe ter de ser abstrata
+                não pode ser instanciada */
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+}
