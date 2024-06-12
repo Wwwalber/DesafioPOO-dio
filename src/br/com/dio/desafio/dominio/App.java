caae8ef63c78a010c8c9fbe887ac897e2097aef7
Walber <walberoc@gmail.com>
2024-06-12 07:15:18 -0300

    testando as classes

---
+++ src/br/com/dio/desafio/dominio/App.java
@@ -0,0 +1,26 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Cursos Java");
+        curso1.setDescricao("Descrição do curso de Java.");
+        curso1.setCargaHoraria(8);    
+        System.out.println(curso1);
+        
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso de JavaScript");
+        curso2.setDescricao("Descrição do curso de Java.");
+        curso2.setCargaHoraria(8);  
+        System.out.println(curso2);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("mentoria de java com Spring Boot");
+        mentoria1.setData(LocalDate.now());
+        System.out.println(mentoria1);
+
+    }
+}
