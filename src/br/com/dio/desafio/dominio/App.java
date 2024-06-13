4248da7a810b89ce760866aa1f514b94a740ee51
Walber <walberoc@gmail.com>
2024-06-12 22:46:52 -0300

    testando inscrição, progredir, cursos inscritos, cursos concluidos

--- src/br/com/dio/desafio/dominio/App.java
+++ src/br/com/dio/desafio/dominio/App.java
@@ -18,12 +18,41 @@ public class App {
 
         Conteudo conteudo = new Curso();/* com polimorfismo posso instanciar
                                     Conteudo mais a partir de curso */
-
+        
         Mentoria mentoria1 = new Mentoria();
         mentoria1.setTitulo("Mentoria de Java");
         mentoria1.setDescricao("mentoria de java com Spring Boot");
         mentoria1.setData(LocalDate.now());
         System.out.println(mentoria1);
+
+        /* instanciar um bootcamp */
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição do Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+        
+        /* instanciar dois devs */
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Camila "+devCamila.getConteudosInscritos());
+
+        devCamila.progredir();// concluiu um conteúdo
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos Camila "+devCamila.getConteudosInscritos());
+        System.out.println("Conteúdos concluidos Camila "+devCamila.getConteudosConcluidos());
+                System.out.println("----------");
+        Dev devWalber = new Dev();
+        devWalber.setNome("Walber");
+        devWalber.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Walber "+devWalber.getConteudosInscritos());
+        devWalber.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos Walber "+devWalber.getConteudosInscritos());
+        System.out.println("Conteúdos concluidos Walber "+devWalber.getConteudosConcluidos());
+
 
     }
 }
